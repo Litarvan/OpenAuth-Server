619daa5291fbf54a8b1b93428e58a28fca2bd917
TheShark34 <adrien1975@live.fr>
2015-05-18 22:11:54 +0200

    Code stylism setp 3/14

--- server/App/logout.php
+++ server/App/logout.php
@@ -18,29 +18,60 @@
 * along with OpenAuth.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-if($request['method'] == "POST"){
-	if($request['content-type'] == "application/json"){
+// If the request method is POST
+if($request['method'] == "POST")
+	// If the content-type is JSON
+	if($request['content-type'] == "application/json") {
+		// Getting the input JSON
 		$input = file_get_contents("php://input");
+
+		// Decoding the JSON
 		$getContents = json_decode($input, true);
+
+		// Getting the given username
 		$username = !empty($getContents['username']) ? $getContents['username'] : null;
+
+		// Getting the given password
 		$password = !empty($getContents['password']) ? $getContents['password'] : null;
-		if(!is_null($username) & !is_null($password)){
+
+		// If they aren't null
+		if(!is_null($username) & !is_null($password)) {
+			// Sending a request to the database to get the user from his username and his password
 			$req = Core\Queries::execute('SELECT * FROM openauth_users WHERE username=:username', ['username' => $username]);
-			if(!empty($req)){
+
+			// If the user was found (the request response isn't empty)
+			if(!empty($req)) {
+				// Hashing the password
 				$password = hash('sha256', $password);
-				if($password == $req->password){
+
+				// If the password is the same as the one of the database
+				if($password == $req->password)
+					// Sending a request to the database to delete the user's access token
 					Core\Queries::execute('UPDATE openauth_users SET accessToken=:accessToken WHERE username=:username', ['username' => $username, 'accessToken' => null]);
-					echo null;
-				}else{
+
+				// Else if the password aren't the same
+				else
+					// Returning the third error
 					echo error(3);
-				}
 			}
-		}else{
+
+			// Else if the request is empty (the user wasn't found)
 			echo error(3);
 		}
-	}else{
-		echo error(6);
+
+		// Else if one of them is null
+		else
+			// Returning the third error
+			echo error(3);
+		
 	}
-}else{
+
+	// Else if the content-type isn't JSON
+	else
+		// Returning the sixth error
+		echo error(6);
+	
+// Else if the request method isn't POST
+else
+	// Retruning the first error
 	echo error(1);
-}
